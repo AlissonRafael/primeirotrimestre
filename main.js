857fca809ac63979a263618488cb25eb2eb25392
Alisson Rafael <132589755+AlissonRafael@users.noreply.github.com>
2024-04-09 10:48:26 -0300

    ijklj

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
     botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
@@ -14,10 +15,11 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+contadores[0].textContent = calculaTempo(tempos[0]);
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
